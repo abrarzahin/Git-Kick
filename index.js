0d59452df7430ebd00f0a0595979d46a640589d6
abrar zahin <abrarzahin@yahoo.com>
2019-08-22 23:31:26 -0400

    interface for  CLI

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const kick = require('commander');
+
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear');
 
 const files = require('./lib/files');
+
+kick 
+	.command('init')
+
+	.description('Draw app banner')
+
+	.action(() => {
+		clear();
+		console.log(chalk.magenta(figlet.textSync('kick', { horizontalLayout: 'full'})));
+	}); 
+
+kick.parse(process.argv);
+if(!kick.args.length){
+	kick.help();
+}
