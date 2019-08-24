3813f2f0e7495b6ccff47ca2e35b4e5e7d489e5c
abrar zahin <abrarzahin@yahoo.com>
2019-08-24 00:19:08 -0400

    Initial commit

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const clear = require('clear');
 
 const files = require('./lib/files');
 const github = require('./lib/github_credentials');
+const inquirer = require('./lib/inquirer');
+const repo = require('./lib/create_a_repo');
 
 kick 
 	.command('init')
@@ -27,6 +29,52 @@ kick
 			token = await github.registerNewToken();
 		}
 		console.log(token);
+	});
+
+
+kick
+	.command('create_repo')
+	.description('Create a new repository on GitHub')
+	.action(async() => {
+		const getGitHubToken = async () => {
+			let token = github.getStoredGitHubToken();
+			if (token) {
+				return token;
+			}
+
+			await github.setGitHubCredentials();
+
+			token = await github.registerNewToken();
+			return token;
+		}
+		try {
+			const token = await getGitHubToken();
+			github.gitHubAuth(token);
+
+			const url = await repo.createRemoteRepository();
+
+			await repo.createGitIgnore();
+
+			const complete = await repo.setupRepository(url);
+
+			if (complete) {
+				console.log(chalk.green('All done!'));
+				}
+			} catch (error) {
+			if (error) {
+				switch (error.status) {
+					case 401: 
+						console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials or token.'));
+						break;
+					case 422:
+						console.log(chalk.red('There already exists a remote repository with the same name.'));
+						break;
+					default:
+						console.log(error);
+						break;
+				}
+			}
+		}
 	});
 
 kick.parse(process.argv);
