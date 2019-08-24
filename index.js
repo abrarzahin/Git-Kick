1e9fbd80c7986b73aa601c1626ee8e31a88fb5d3
abrar zahin <abrarzahin@yahoo.com>
2019-08-23 23:19:32 -0400

    github token implimentation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const figlet = require('figlet');
 const clear = require('clear');
 
 const files = require('./lib/files');
+const github = require('./lib/github_credentials');
 
 kick 
 	.command('init')
@@ -15,6 +16,18 @@ kick
 		clear();
 		console.log(chalk.magenta(figlet.textSync('kick', { horizontalLayout: 'full'})));
 	}); 
+ 
+ kick
+	.command('octocheck')
+	.description('Check user GitHub credentials')
+	.action(async () => {
+		let token = github.getStoredGitHubToken();
+		if (!token) {
+			await github.setGitHubCredentials();
+			token = await github.registerNewToken();
+		}
+		console.log(token);
+	});
 
 kick.parse(process.argv);
 if(!kick.args.length){
